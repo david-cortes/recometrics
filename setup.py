7b36a254137cd2588c607aa5bda352b259a5a04f
David Cortes <david.cortes.rivera@gmail.com>
2022-05-21 20:52:54 +0200

    fixes for apple setups, more alternatives for march

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ class build_ext_subclass( build_ext ):
             for e in self.extensions:
                 e.extra_compile_args += ['/O2', '/openmp', '/std:c++14', '/fp:contract', '/fp:except-']
         else:
-            if not self.check_cflags_or_cxxflags_contain_arch():
+            if not self.check_for_variable_dont_set_march() and not self.check_cflags_or_cxxflags_contain_arch():
                 self.add_march_native()
             else:
                 for e in self.extensions:
@@ -28,15 +28,16 @@ class build_ext_subclass( build_ext ):
             self.add_restrict_qualifier()
             self.add_no_math_errno()
             self.add_no_trapping_math()
+            self.add_O3()
+            if not is_clang:
+                self.add_std_cpp11()
 
             for e in self.extensions:
                 # e.extra_compile_args += ['-O3', '-fopenmp', '-march=native', '-std=c++11']
                 # e.extra_link_args += ['-fopenmp']
 
                 if is_clang:
-                    e.extra_compile_args += ['-O3', '-std=c++17']
-                else:
-                    e.extra_compile_args += ['-O3', '-std=c++11']
+                    e.extra_compile_args += ['-std=c++17']
 
                 # e.extra_compile_args += ['-O3', '-fopenmp', '-march=native', '-std=c++17']
                 # e.extra_link_args += ['-fopenmp=libiomp5']
@@ -62,6 +63,9 @@ class build_ext_subclass( build_ext ):
                         return True
         return False
 
+    def check_for_variable_dont_set_march(self):
+        return "DONT_SET_MARCH" in os.environ
+
     def add_march_native(self):
         arg_march_native = "-march=native"
         arg_mcpu_native = "-mcpu=native"
@@ -86,36 +90,56 @@ class build_ext_subclass( build_ext ):
                 e.extra_compile_args.append(arg_fntm)
                 e.extra_link_args.append(arg_fntm)
 
+    def add_O3(self):
+        arg_O3 = "-O3"
+        if self.test_supports_compile_arg(arg_O3):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_O3)
+                e.extra_link_args.append(arg_O3)
+
+    def add_std_cpp11(self):
+        arg_std_cpp11 = "-std=c++11"
+        if self.test_supports_compile_arg(arg_std_cpp11):
+            for e in self.extensions:
+                e.extra_compile_args.append(arg_std_cpp11)
+                e.extra_link_args.append(arg_std_cpp11)
+
     def add_openmp_linkage(self):
         arg_omp1 = "-fopenmp"
         arg_omp2 = "-qopenmp"
         arg_omp3 = "-xopenmp"
         arg_omp4 = "-fiopenmp"
         args_apple_omp = ["-Xclang", "-fopenmp", "-lomp"]
-        if self.test_supports_compile_arg(arg_omp1):
+        args_apple_omp2 = ["-Xclang", "-fopenmp", "-L/usr/local/lib", "-lomp", "-I/usr/local/include"]
+        if self.test_supports_compile_arg(arg_omp1, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp1)
                 e.extra_link_args.append(arg_omp1)
-        elif (sys.platform[:3].lower() == "dar") and self.test_supports_compile_arg(args_apple_omp):
+        elif (sys.platform[:3].lower() == "dar") and self.test_supports_compile_arg(args_apple_omp, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args += ["-Xclang", "-fopenmp"]
                 e.extra_link_args += ["-lomp"]
-        elif self.test_supports_compile_arg(arg_omp2):
+        elif (sys.platform[:3].lower() == "dar") and self.test_supports_compile_arg(args_apple_omp2, with_omp=True):
+            for e in self.extensions:
+                e.extra_compile_args += ["-Xclang", "-fopenmp"]
+                e.extra_link_args += ["-L/usr/local/lib", "-lomp"]
+                e.include_dirs += ["/usr/local/include"]
+        elif self.test_supports_compile_arg(arg_omp2, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp2)
                 e.extra_link_args.append(arg_omp2)
-        elif self.test_supports_compile_arg(arg_omp3):
+        elif self.test_supports_compile_arg(arg_omp3, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp3)
                 e.extra_link_args.append(arg_omp3)
-        elif self.test_supports_compile_arg(arg_omp4):
+        elif self.test_supports_compile_arg(arg_omp4, with_omp=True):
             for e in self.extensions:
                 e.extra_compile_args.append(arg_omp4)
                 e.extra_link_args.append(arg_omp4)
         else:
             set_omp_false()
 
-    def test_supports_compile_arg(self, comm):
+    def test_supports_compile_arg(self, comm, with_omp=False):
         is_supported = False
         try:
             if not hasattr(self.compiler, "compiler_cxx"):
@@ -127,10 +151,16 @@ class build_ext_subclass( build_ext ):
             with open(fname, "w") as ftest:
                 ftest.write(u"int main(int argc, char**argv) {return 0;}\n")
             try:
-                cmd = [self.compiler.compiler_cxx[0]]
+                if not isinstance(self.compiler.compiler_cxx, list):
+                    cmd = list(self.compiler.compiler_cxx)
+                else:
+                    cmd = self.compiler.compiler_cxx
             except:
-                cmd = list(self.compiler.compiler_cxx)
+                cmd = self.compiler.compiler_cxx
             val_good = subprocess.call(cmd + [fname])
+            if with_omp:
+                with open(fname, "w") as ftest:
+                    ftest.write(u"#include <omp.h>\nint main(int argc, char**argv) {return 0;}\n")
             try:
                 val = subprocess.call(cmd + comm + [fname])
                 is_supported = (val == val_good)
@@ -154,9 +184,12 @@ class build_ext_subclass( build_ext ):
             with open(fname, "w") as ftest:
                 ftest.write(u"int main(int argc, char**argv) {return 0;}\n")
             try:
-                cmd = [self.compiler.compiler_cxx[0]]
+                if not isinstance(self.compiler.compiler_cxx, list):
+                    cmd = list(self.compiler.compiler_cxx)
+                else:
+                    cmd = self.compiler.compiler_cxx
             except:
-                cmd = list(self.compiler.compiler_cxx)
+                cmd = self.compiler.compiler_cxx
             val_good = subprocess.call(cmd + [fname])
             try:
                 with open(fname, "w") as ftest:
@@ -181,7 +214,7 @@ class build_ext_subclass( build_ext ):
 setup(
     name  = "recometrics",
     packages = ["recometrics"],
-    version = '0.1.6-1',
+    version = '0.1.6-2',
     cmdclass = {'build_ext': build_ext_subclass},
     author = 'David Cortes',
     author_email = 'david.cortes.rivera@gmail.com',
